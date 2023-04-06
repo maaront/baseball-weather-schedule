dab7a7891b7048eadf0bd8075e0238cac3841afd
maaront <35611834+maaront@users.noreply.github.com>
2023-04-05 23:04:17 -0400

    pull sessionStorage data and create new URL and webpage

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
-const scheduleURL = "https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&startDate=2023-03-01&endDate=2023-12-29";
-const apiKey = "AIzaSyDlAGwmtyyL0LvNpxgoBbkgVJfElCJaz2g";
+// const scheduleURL = "https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&startDate=2023-03-01&endDate=2023-12-29";
+// const apiKey = "AIzaSyDlAGwmtyyL0LvNpxgoBbkgVJfElCJaz2g";
 
 
 
@@ -15,9 +15,39 @@ teamEls.forEach(element => {
     // Save the ID to session storage
     sessionStorage.setItem("team-abbreviation", id);
 
-  });
+// Get the data you want to use in the URL from session storage
+const data = sessionStorage.getItem("team-abbreviation");
+
+// Generate the URL for the new webpage
+const url = `https://maaront.github.io/baseball-weather-schedule/?data=${encodeURIComponent(data)}`;
+
+// Create a new HTML file
+const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>New Page</title>
+    </head>
+    <body>
+      <h1>New Page</h1>
+      <p>Data from session storage: ${data}</p>
+    </body>
+  </html>
+`;
+
+// Create a Blob object from the HTML string
+const blob = new Blob([html], { type: "text/html" });
+
+// Create a URL object from the Blob object
+const urlObject = URL.createObjectURL(blob);
+
+// Navigate to the new webpage using the generated URL
+window.location.href = urlObject;
+
+
+});
 });
 
 
 // Construct the logo URL using the team ID
-const logoURL = `https://a.espncdn.com/i/teamlogos/mlb/500/${teamData.abbreviation}.png`;
+// const logoURL = `https://a.espncdn.com/i/teamlogos/mlb/500/${teamData.abbreviation}.png`;
