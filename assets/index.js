f962f7f97b7f71c916583d0194ad5984b3823453
maaront <35611834+maaront@users.noreply.github.com>
2023-04-06 08:52:50 -0400

    migrate team-script.js and add team page html

--- assets/index.js
+++ assets/index.js
@@ -15,24 +15,48 @@ teamEls.forEach(element => {
     // Save the ID to session storage
     sessionStorage.setItem("team-abbreviation", id);
 
-// Get the data you want to use in the URL from session storage
+// Get the ID from session storage
 const data = sessionStorage.getItem("team-abbreviation");
 
-// Generate the URL for the new webpage
-const url = `https://maaront.github.io/baseball-weather-schedule/?data=${encodeURIComponent(data)}`;
+// // Generate the URL for the new webpage
+// const url = `https://maaront.github.io/baseball-weather-schedule/?data=${encodeURIComponent(data)}`;
 
 // Create a new HTML file
 const html = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>New Page</title>
-    </head>
-    <body>
-      <h1>New Page</h1>
-      <p>Data from session storage: ${data}</p>
-    </body>
-  </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Baseball Weather</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+</head>
+<body>
+  <h1>Baseball Weather</h1>
+  <div class="team-info">
+    <img class="team-logo" alt="Team Logo">
+    <h2 id="team-name"></h2>
+  </div>
+  <ul id="games-list"></ul>
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="team-script.js"></script>
+  <div id="weather-modal" class="modal">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <h2 class="title is-4" id="weather-title">Weather Information</h2>
+        <p id="weather-description"></p>
+        <p id="weather-temperature"></p>
+        <p id="weather-wind-speed"></p>
+        <p id="weather-humidity"></p>
+        <p id="weather-rain-chance"></p>
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close"></button>
+  </div>
+</body>
+</html>
 `;
 
 // Create a Blob object from the HTML string
@@ -51,3 +75,5 @@ window.location.href = urlObject;
 
 // Construct the logo URL using the team ID
 // const logoURL = `https://a.espncdn.com/i/teamlogos/mlb/500/${teamData.abbreviation}.png`;
+
+
